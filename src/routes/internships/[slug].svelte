9da1b8df663c88027d20b9c99764fbb713e3cbac
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-05-31 11:51:48 +0300

    update internships

--- src/routes/internships/[slug].svelte
+++ src/routes/internships/[slug].svelte
@@ -12,7 +12,7 @@
 </script>
 
 <script>
-	import {Button, Icon, Snackbar} from "svelte-mui/src"
+	import {Button, Icon, Snackbar, Textfield} from "svelte-mui/src"
 	import {user} from "../../store"
 	import Spinner from "../../components/spinner.svelte"
 
@@ -20,7 +20,25 @@
 	let visible = false,
 		loading = false,
 		message,
-		snackbar_color
+		snackbar_color, promise, comment
+
+    promise = get_comments()
+
+	async function get_comments() {
+		try {
+			const response = await custom_axios.get(
+				`posts/internship/${internship.id}/comments`
+			)
+
+			if (response.status === 200 || response.status === 201) {
+			    return response.data
+			}
+		} catch (e) {
+			message = `${e.response.data}`
+			snackbar_color = "red"
+			visible = true
+		}
+	}
 
 	async function like() {
 		try {
@@ -38,6 +56,30 @@
 			}
 		} catch (e) {
 			message = e.response.data.error
+			snackbar_color = "red"
+			visible = true
+			loading = false
+		}
+	}
+
+		async function post_comment() {
+		try {
+			loading = true
+            const data = {comment: comment}
+			const response = await custom_axios.post(
+				`posts/internship/${internship.id}/comment`, data
+			)
+            console.log(response)
+			if (response.status === 200 || response.status === 201) {
+				message = "comment created Successfully"
+				snackbar_color = "green"
+				visible = true
+				loading = false
+                promise = get_comments()
+			}
+		} catch (e) {
+		    console.log(e.response)
+			message = e.response
 			snackbar_color = "red"
 			visible = true
 			loading = false
@@ -136,5 +178,222 @@
 				</div>
 			</div>
 		</div>
+	</div>
+</section>
+<br />
+<br />
+<br />
+<br />
+<h1>Applied</h1>
+<hr />
+<section class="text-gray-700 body-font">
+	<div class="container px-5 py-24 mx-auto">
+		<div class="flex flex-wrap -m-4">
+			{#each internship.applied_users as {avatarImg, email, username, fullname}}
+				<div
+					class="container mx-auto max-w-sm rounded-lg overflow-hidden
+					shadow-lg my-2 bg-white">
+					<div
+						class="relative z-9"
+						style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0
+						calc(100% - 5vw));">
+						<img
+							class="w-full"
+							src="{avatarImg}"
+							alt="{fullname} Profile" />
+						<div
+							class="text-center absolute w-full"
+							style="bottom: 4rem">
+							<p
+								class="text-white tracking-wide uppercase text-lg
+								font-bold">
+								{fullname}
+							</p>
+							<p class="text-gray-400 text-sm">{username}</p>
+						</div>
+					</div>
+					<div
+						class="relative flex justify-between items-center
+						flex-row px-6 z-11 -mt-10">
+						<p class="flex items-center text-gray-400">
+							<span
+								class="inline-block w-3 h-3 bg-green-500
+								rounded-full mr-2"></span>
+							{email}
+						</p>
+						<a href="users/{username}/">
+							<button
+								class="p-4 bg-red-600 rounded-full
+								hover:bg-red-500 focus:bg-red-700 transition
+								ease-in duration-200 focus:outline-none">
+								<svg
+									viewBox="0 0 20 20"
+									enable-background="new 0 0 20 20"
+									class="w-6 h-6">
+									<path
+										fill="#FFFFFF"
+										d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
+								</svg>
+							</button>
+						</a>
+					</div>
+					<div class="pt-6 pb-8 text-gray-600 text-center">
+						<p>Fullstack Developer</p>
+						<p class="text-sm">Coding from Planet Earth</p>
+					</div>
+
+					<div
+						class="pb-10 uppercase text-center tracking-wide flex
+						justify-around">
+						<div class="posts">
+							<p class="text-gray-400 text-sm">Posts</p>
+							<p class="text-lg font-semibold text-blue-300">76</p>
+						</div>
+						<div class="followers">
+							<p class="text-gray-400 text-sm">Followers</p>
+							<p class="text-lg font-semibold text-blue-300">
+								964
+							</p>
+						</div>
+						<div class="following">
+							<p class="text-gray-400 text-sm">Following</p>
+							<p class="text-lg font-semibold text-blue-300">34</p>
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
+
+<br />
+<h1>Accepted</h1>
+<hr />
+<section class="text-gray-700 body-font">
+	<div class="container px-5 py-24 mx-auto">
+		<div class="flex flex-wrap -m-4">
+			{#each internship.accepted_users as {avatarImg, email, username, fullname}}
+				<div
+					class="container mx-auto max-w-sm rounded-lg overflow-hidden
+					shadow-lg my-2 bg-white">
+					<div
+						class="relative z-9"
+						style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0
+						calc(100% - 5vw));">
+						<img
+							class="w-full"
+							src="{avatarImg}"
+							alt="{fullname} Profile" />
+						<div
+							class="text-center absolute w-full"
+							style="bottom: 4rem">
+							<p
+								class="text-white tracking-wide uppercase text-lg
+								font-bold">
+								{fullname}
+							</p>
+							<p class="text-gray-400 text-sm">{username}</p>
+						</div>
+					</div>
+					<div
+						class="relative flex justify-between items-center
+						flex-row px-6 z-11 -mt-10">
+						<p class="flex items-center text-gray-400">
+							<span
+								class="inline-block w-3 h-3 bg-green-500
+								rounded-full mr-2"></span>
+							{email}
+						</p>
+						<a href="users/{username}/">
+							<button
+								class="p-4 bg-red-600 rounded-full
+								hover:bg-red-500 focus:bg-red-700 transition
+								ease-in duration-200 focus:outline-none">
+								<svg
+									viewBox="0 0 20 20"
+									enable-background="new 0 0 20 20"
+									class="w-6 h-6">
+									<path
+										fill="#FFFFFF"
+										d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
+								</svg>
+							</button>
+						</a>
+					</div>
+					<div class="pt-6 pb-8 text-gray-600 text-center">
+						<p>Fullstack Developer</p>
+						<p class="text-sm">Coding from Planet Earth</p>
+					</div>
+
+					<div
+						class="pb-10 uppercase text-center tracking-wide flex
+						justify-around">
+						<div class="posts">
+							<p class="text-gray-400 text-sm">Posts</p>
+							<p class="text-lg font-semibold text-blue-300">76</p>
+						</div>
+						<div class="followers">
+							<p class="text-gray-400 text-sm">Followers</p>
+							<p class="text-lg font-semibold text-blue-300">
+								964
+							</p>
+						</div>
+						<div class="following">
+							<p class="text-gray-400 text-sm">Following</p>
+							<p class="text-lg font-semibold text-blue-300">34</p>
+						</div>
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</section>
+
+<br />
+<h1>Comments</h1>
+<hr />
+<section class="text-gray-700 body-font">
+	<div class="container px-5 py-24 mx-auto">
+        <div class="flex">
+        <Textfield
+            filled
+            autocomplete="on"
+            label="Comment"
+            required
+            bind:value="{comment}"
+            type="text" />
+
+            <Button icon on:click={post_comment}>
+            <Icon
+                path="M2.01 21L23 12 2.01 3 2
+                10l15 2-15 2z"/>
+        </Button>
+        </div>
+        <br/>
+        {#await promise}
+        <h1>Loading.......</h1>
+            {:then data}
+            {#each data.comments as {comment, userId}}
+        <div
+			class="flex flex-col bg-white px-8 py-6 max-w-sm mx-auto rounded-lg
+			shadow-lg">
+			<div class="mt-4">
+				<a class="text-lg text-gray-700 font-medium" href="/">
+                    {comment}
+				</a>
+			</div>
+                    <div class="flex justify-between items-center mt-4">
+            <div class="flex items-center">
+                <img src="https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                     class="w-8 h-8 object-cover rounded-full" alt="avatar">
+                <a class="text-gray-700 text-sm mx-3" href="/users/{userId}">{userId}</a>
+            </div>
+            <span class="font-light text-sm text-gray-600">Mar 10, 2018</span>
+        </div>
+		</div>
+		<br />
+    {/each}
+        {/await}
+
 	</div>
 </section>
--- src/routes/internships/index.svelte
+++ src/routes/internships/index.svelte
@@ -10,83 +10,200 @@
 
 <script>
 	export let internships
+	import {Icon, Dialog, Datefield, Textfield, Button} from "svelte-mui/src"
+
+	let salarys,
+		durations,
+		candidates,
+		descriptions,
+		expired_dates,
+		statuss,
+		locations,
+		types,
+		titles,
+		field_majors,
+		visible = false
+	async function create() {
+		const data = {
+			salary: salarys,
+			duration: durations,
+			number_of_candidates: candidates,
+			description: descriptions,
+			expiredation: expired_dates,
+			status: statuss,
+			location: locations,
+			type: types,
+			title: titles,
+			field_major: field_majors,
+		}
+		try {
+			const response = await custom_axios.post("posts/internship", data)
+			console.log(response.data)
+		} catch (e) {
+			console.log(e.response.data.error)
+		}
+	}
 </script>
 
 <svelte:head>
 	<title>List of Internships | Relay</title>
 </svelte:head>
 
+<Dialog width="1000" bind:visible>
+	<div slot="title">Create New Internship!</div>
+
+	<form on:submit|preventDefault="{create}">
+		<Textfield
+			fill
+			type="text"
+			required
+			bind:value="{titles}"
+			label="Title" />
+		<Textfield
+			fill
+			type="number"
+			required
+			bind:value="{salarys}"
+			label="Salary" />
+		<Textfield
+			fill
+			type="text"
+			required
+			bind:value="{durations}"
+			label="Duration" />
+		<Textfield
+			fill
+			type="number"
+			required
+			bind:value="{candidates}"
+			label="candidates" />
+		<Datefield
+			value="{expired_dates}"
+			label="DeadLine"
+			format="D.MM.YYYY"
+			message="D.MM.YYYY"
+			locale="ar"
+			icon />
+		<Textfield
+			fill
+			type="text"
+			required
+			bind:value="{statuss}"
+			label="Status" />
+		<Textfield
+			fill
+			type="text"
+			required
+			bind:value="{locations}"
+			label="Location" />
+		<Textfield fill type="text" required bind:value="{types}" label="Type" />
+		<Textfield
+			fill
+			type="text"
+			required
+			bind:value="{field_majors}"
+			label="Major" />
+		<Textfield
+			fill
+			type="text"
+			required
+			bind:value="{descriptions}"
+			label="Description" />
+		<Button raised shaped type="submit" color="primary">
+			Submit
+			<Icon
+				path="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"
+				style="margin: 0 -4px 0 8px;" />
+		</Button>
+	</form>
+</Dialog>
+
 <section class="text-gray-700 body-font">
 	<div class="container px-5 py-24 mx-auto">
+
+		<Button raised on:click="{() => (visible = true)}">Create new</Button>
+
 		{#each internships as {type, title, status, number_of_candidates, location, expiredation, duration, id, likes}}
-			<div class="lg:flex shadow rounded-lg border border-gray-400">
-				<div
-					class="bg-red-600 rounded-lg lg:w-2/12 py-4 block h-full
-					shadow-inner">
-					<div class="text-center tracking-wide">
-						<div class="text-white font-bold text-2xl ">
-							DeadLine
-						</div>
-						<div class="text-white font-normal text-2xl">
-							{expiredation}
-						</div>
-					</div>
-				</div>
-				<div
-					class="w-full lg:w-11/12 xl:w-full px-1 bg-white py-5 lg:px-2
-					lg:py-2 tracking-wide">
-					<div class="flex flex-row lg:justify-start justify-center">
-						<div
-							class="text-gray-700 font-medium text-sm text-center
-							lg:text-left px-2">
-							<i class="far fa-clock"></i>
-							<strong>Duration</strong>
-							: {duration}
-						</div>
-						<div
-							class="text-gray-700 font-medium text-sm text-center
-							lg:text-left px-2">
-							<strong>Likes</strong>
-							: {likes}
-						</div>
-						<div
-							class="text-gray-700 font-medium text-sm text-center
-							lg:text-left px-2">
-							<strong>Candidates</strong>
-							: {number_of_candidates}
-						</div>
-					</div>
-					<a href="internships/{id}/">
-						<div
-							class="font-semibold text-blue-800 text-xl
-							text-center lg:text-left px-2">
-							{title}
-						</div>
-					</a>
-
-					<div
-						class="text-gray-600 font-medium text-sm pt-1 text-center
-						lg:text-left px-2">
-						{location}
-					</div>
-					<div
-						class="text-gray-600 font-medium text-sm pt-1 text-center
-						lg:text-left px-2">
-						{type}
-					</div>
-				</div>
-				<div
-					class="flex flex-row items-center w-full lg:w-1/3 bg-white
-					lg:justify-end justify-center px-2 py-4 lg:px-0">
-					<span
-						class="tracking-wider text-gray-600 bg-gray-200 px-2
-						text-sm rounded leading-loose mx-2 font-semibold">
+			<div class="bg-white shadow-lg rounded-lg overflow-hidden my-4">
+				<div class="flex items-center px-6 py-3 bg-gray-900">
+					<svg
+						class="h-6 w-6 text-white fill-current"
+						viewBox="0 0 512 512">
+						<path
+							d="M256 48C150 48 64 136.2 64 245.1v153.3c0 36.3 28.6
+							65.7 64 65.7h64V288h-85.3v-42.9c0-84.7 66.8-153.3
+							149.3-153.3s149.3 68.5 149.3
+							153.3V288H320v176h64c35.4 0 64-29.3 64-65.7V245.1C448
+							136.2 362 48 256 48z"></path>
+					</svg>
+					<h1 class="mx-3 text-white font-semibold text-lg">
 						{status}
-					</span>
-
+					</h1>
+				</div>
+				<div class="py-4 px-6">
+					<a href="/internships/{id}/">
+						<h1 class="text-2xl font-semibold text-blue-800">
+							{title}
+						</h1>
+					</a>
+					<p class="py-2 text-lg text-gray-700">
+						DeadLine: {expiredation}
+						<br />
+						Duration: {duration}
+						<br />
+						Likes: {likes}
+						<br />
+						Type: {type}
+					</p>
+					<div class="flex items-center mt-4 text-gray-700">
+						<svg class="h-6 w-6 fill-current" viewBox="0 0 512 512">
+							<path
+								d="M239.208 343.937c-17.78 10.103-38.342
+								15.876-60.255 15.876-21.909
+								0-42.467-5.771-60.246-15.87C71.544 358.331 42.643
+								406 32
+								448h293.912c-10.639-42-39.537-89.683-86.704-104.063zM178.953
+								120.035c-58.479 0-105.886 47.394-105.886 105.858
+								0 58.464 47.407 105.857 105.886
+								105.857s105.886-47.394
+								105.886-105.857c0-58.464-47.408-105.858-105.886-105.858zm0
+								186.488c-33.671
+								0-62.445-22.513-73.997-50.523H252.95c-11.554
+								28.011-40.326 50.523-73.997 50.523z"></path>
+							<g>
+								<path
+									d="M322.602
+									384H480c-10.638-42-39.537-81.691-86.703-96.072-17.781
+									10.104-38.343 15.873-60.256 15.873-14.823
+									0-29.024-2.654-42.168-7.49-7.445 12.47-16.927
+									25.592-27.974 34.906C289.245 341.354 309.146
+									364 322.602 384zM306.545 200h100.493c-11.554
+									28-40.327 50.293-73.997 50.293-8.875
+									0-17.404-1.692-25.375-4.51a128.411 128.411 0
+									0 1-6.52 25.118c10.066 3.174 20.779 4.862
+									31.895 4.862 58.479 0 105.886-47.41
+									105.886-105.872
+									0-58.465-47.407-105.866-105.886-105.866-37.49
+									0-70.427 19.703-89.243 49.09C275.607 131.383
+									298.961 163 306.545 200z"></path>
+							</g>
+						</svg>
+						<h1 class="px-2 text-sm">{number_of_candidates}</h1>
+					</div>
+					<div class="flex items-center mt-4 text-gray-700">
+						<svg class="h-6 w-6 fill-current" viewBox="0 0 512 512">
+							<path
+								d="M256 32c-88.004 0-160 70.557-160 156.801C96
+								306.4 256 480 256 480s160-173.6 160-291.199C416
+								102.557 344.004 32 256 32zm0 212.801c-31.996
+								0-57.144-24.645-57.144-56 0-31.357 25.147-56
+								57.144-56s57.144 24.643 57.144 56c0 31.355-25.148
+								56-57.144 56z"></path>
+						</svg>
+						<h1 class="px-2 text-sm">{location}</h1>
+					</div>
 				</div>
 			</div>
-			<br />
 		{/each}
 	</div>
 </section>
